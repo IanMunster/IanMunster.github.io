6a361e70550043121f42e961118f14149fafb7c5
i-Jun <Munster@live.nl>
2022-07-25 01:19:22 +0200

    Continued Update
    https://www.freecodecamp.org/news/how-to-make-a-landing-page-with-html-css-and-javascript/#howtomakethedarkandlightthemeswitcher

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 
+/* Hamburger Menu Functionality for Small Screens */
+	// Navigation HamburgerMenu Selections
+	const burger = document.querySelector("#BurgerBtn"),
+		navLink = document.querySelectorAll(".navBarLink"),
+		ul = document.querySelector("nav ul"),
+		nav = document.querySelector("nav");
+
+	// Open Menu on Click
+	burger.addEventListener("click", () => {
+		ul.classList.toggle("show");
+	});
+
+	// Close Menu on Click
+	navLink.forEach((link) => 
+		link.addEventListener("click", () => {
+			ul.classList.remove("show");
+		})
+	);
+	/* END Hamburger Menu Functionality for Small Screens */
+
 // Scroll to Top Function
 const scrollUp = document.querySelector("#ScrollUp");
 	scrollUp.addEventListener("click", () => {
