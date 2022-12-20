8bdd04471b1cbb2627fc170f7988655008c158bc
I-Jun <109482772+I-Jun@users.noreply.github.com>
2022-12-20 17:20:47 +0100

    Added Lightbox/Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,9 @@ themeBtn.addEventListener('click', SwitchThemeUpdate);
 
 /** SwitchThemeUpdate:
  * Updates the CSS variable value to Switch Themes.
- * @param  {event} e [Click event on Switch Btns]
- * @return {null}    [Does not return a value (?)]
+ * @param  {event} e [Click event on Switch Buttons]
  */
 function SwitchThemeUpdate(e) {
-/* 	console.log ("themeBtn.checked: "+ themeBtn.checked ) */
 	if (themeBtn.checked == true) {
 /* 		console.log ("Light Theme") */
 		root.style.setProperty('--bg-color0', '#dedcd9');
@@ -118,13 +116,61 @@ for (var i = 0; i < filterBtns.length; i++) {
 
 /* END Read More Filter Function */
 
+/* Modal Lightbox Functions */
+// Open the Modal
+function openModal() {
+	document.getElementById("GalleryModal").style.display = "block";
+}
+//Close the Modal
+function closeModal() {
+	document.getElementById("GalleryModal").style.display = "none";
+}
+
+// Slides
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous Controls
+function changeSlides(number) {
+	showSlides (slideIndex += number);
+}
+
+// Thumbnail Image Controls
+function currentSlide(number) {
+	showSlides(slideIndex = number);
+}
+
+// Show the Slides on Number Index
+function showSlides(number) {
+	var i,
+	slides = document.getElementsByClassName("modalSlides"),
+	thumbs = document.getElementsByClassName("modalThumb"),
+	captionText = document.getElementById("ModalCaption");
+	// Number bigger than Total Slides, go to First Slide
+	if (number > slides.length) {
+		slideIndex = 1
+	}
+	// Number Slides smaller than 0, got to Last Slide
+	if (number < 1) {
+		slideIndex = slides.length
+	}
+	// Hide all inactive Slides
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	// Set Thumbnail as InActive Style
+	for (i = 0; i < thumbs.length; i++) {
+		thumbs[i].className = thumbs[i].className.replace(" activeSlide", "");
+	}
+	// Display Slide, Set active Tumb and Display Caption
+	slides[slideIndex-1].style.display = "block";
+	thumbs[slideIndex-1].className += " activeSlide";
+	captionText.innerHTML = thumbs[slideIndex-1].alt;
+}
+
+/* END Modal Lightbox Functions */
+
 /* Scroll to Top Function */
-/**
- * [someFunction description]
- * @param  {[type]} arg1 [description]
- * @param  {[type]} arg2 [description]
- * @return {[type]}      [description]
- */
 const scrollUp = document.querySelector("#ScrollUp");
 scrollUp.addEventListener('click', () => {
 	window.scrollTo({
